6bba9614b606c3c4466cb059d6a57f9951083b43
ricgreenwood <rmgreenwood@gmail.com>
2016-10-21 16:49:02 +0100

    latest changes

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,16 +5,28 @@ public class Ball : MonoBehaviour {
 
 	public Paddle paddle;
 	
-	private Vector3 PaddleToBallVector;
+	private Vector3 paddleToBallVector;
+	private bool hasStarted = false;
 
 	// Use this for initialization
 	void Start () {
-		PaddleToBallVector = this.transform.position - paddle.transform.position;
-		print (PaddleToBallVector);
+		paddleToBallVector = this.transform.position - paddle.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (!hasStarted) {
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			if (Input.GetMouseButtonDown(0)) {
+				hasStarted = true;
+				print ("Mouse clicked, launch ball");
+				this.rigidbody2D.velocity = new Vector2 (2f,10f);
+			}
+		} else {
+		
+		}
+		
+		
 		
 	}
 }
